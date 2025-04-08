43c129d886dc36e51cd3dd7410400a4ffccf7082
dheerajpatel3448w <dheerajdominetar3448@gmail.com>
2025-04-08 05:42:31 +0530

    d

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const allowedOrigins = [
   'http://localhost:5173' // Local dev
 ];
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin:allowedOrigins, credentials:true },
 });
 
 app.use(  cors({
